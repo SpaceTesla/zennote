72cf40f92162e38bd7b72115d7c99734616a7e5c
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2025-04-10 14:39:38 +0530

    feat: deployment changes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// Apply bundle analyzer to the next config
+export default withBundleAnalyzer(nextConfig);
